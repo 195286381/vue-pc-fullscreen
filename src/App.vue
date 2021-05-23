84b059a1bbb0abb7f5df0b00299d10c95573a79f
Xzzzzz <195286381@qq.com>
2021-05-24 04:15:54 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="app-wrapper">
+    <div class="app">
+      <router-view />
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  html, body {
+    overflow: hidden;
 
-#nav {
-  padding: 30px;
-}
+  }
+  .app-wrapper {
+    /*overflow: hidden;*/
+    width: 100vw;
+    height: 100vh;
+    background: url('./assets/1.jpeg') no-repeat center / cover;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .app {
+    width: 1920px;
+    height: 1080px;
+  }
 </style>
