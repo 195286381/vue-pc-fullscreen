a2795789cadb103bb285ab62f888b15bb0517d59
Xzzzzz <195286381@qq.com>
2021-05-24 04:33:47 +0800

    添加大屏自适应问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <div class="title-h3">大屏自适应测试(style样式--可以自动缩放) 精确还原设计稿</div>
+    <div style=" height: 50px; margin: 20px 0; line-height: 50px; border: 10px dashed greenyellow; font-size: 40px; color: white; text-align: center;">大屏自适应测试(内联样式-无法自动缩放)</div>
   </div>
 </template>
 
@@ -9,3 +11,14 @@ export default {
   name: 'Home'
 }
 </script>
+<style>
+  .title-h3 {
+    height: 50px;
+    line-height: 50px;
+    border: 10px dashed greenyellow;
+    font-size: 40px;
+    color: white;
+    text-align: center;
+    margin: 20px 0;
+  }
+</style>
